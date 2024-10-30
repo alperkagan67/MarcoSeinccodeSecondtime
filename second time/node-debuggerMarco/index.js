cd1629738f2110750ea93ba27d665fc5c2589437
Alper <hansd4243@gmail.com>
2024-10-30 12:18:04 +0100

    2Commit Vor Hauzsaufgabe

--- second time/node-debuggerMarco/index.js
+++ second time/node-debuggerMarco/index.js
@@ -1,19 +1,17 @@
 // index.js
-
 var items = [1, "2", 3, "four", 5, null, undefined, 6]
 const results = []
 
-items.forEach((item) => {
-    if(typeof item === 'number')
-        if(item % 2 === 0) results.push(item* 2)
+items.foreach((item) => {
+    if (typeof item === 'number')
+        if (item % 2 === 0) results.push(item * 2)
 
-    else if(item === null)
-    console.log("Found a null item")
+        else if (item === null)
+            console.log("Found a null item")
 
-    else
-    {
-        results.push(parseInt(item))
-    }
+        else {
+            result.push(parseInt(item))
+        }
 });
 
 console.log("Original items:", items)
